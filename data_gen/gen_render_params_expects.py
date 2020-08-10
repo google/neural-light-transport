a00949b7f66f06cb22ce029a651c51a0e3e255a9
Xiuming Zhang <xiuming6zhang@gmail.com>
2020-08-09 18:25:19 -0700

    addlicense

--- data_gen/gen_render_params_expects.py
+++ data_gen/gen_render_params_expects.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """An example script that generates task parameters (to be fed to `render.py`).
 Each set of parameters is a light-camera configuration. These tasks can then be
 distributed over many machines.
